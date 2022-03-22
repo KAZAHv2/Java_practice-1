2b8a6af4e2450be4ac3b862f6dcd43d0da31dc06
eSquire-qq <eSquire.github.repos@gmail.com>
2022-03-22 16:43:19 +0200

    Some small changes

--- Ballistic/src/com/company/Calc.java
+++ Ballistic/src/com/company/Calc.java
@@ -33,18 +33,47 @@ public class Calc {
         return result;
     }
 
-    /** Стала коефіцієнту вільного падіння */
-
-
     /** Вираховує значення формули
      * @param Speed - швидкість польоту
      * @param Angle - кут польоту
+     * @param G - константа(коефіцієнт вільного польоту)
      * */
-    private double calc(double Speed, double Angle){
-
+    private double calc(double Speed, double Angle, double G){
         return Speed * Speed * Math.sin(2 * Angle)/G;
-
     }
 
+    /**Вираховує значення формули
+     * та зберігає рехультат в об'єкт {@linkplain Calc#result}
+     * @param Speed - швидкість польоту
+     * @param Angle - кут польоту
+     * @param G - константа(коефіцієнт вільного польоту)
+     * */
+    //TODO result and return
+    public double init(double Speed, double Angle, double G){
+        result.setSpeed(Speed);
+        result.setAngle(Angle);
+        result.setG(G);
+        return
+    }
+
+    /**Виведення результату обчислення */
+    public void show(){
+        System.out.print(result);
+    }
+
+    public void save() throws IOException {
+        ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(FNAME));
+        os.writeObject(result);
+        os.flush();
+        os.close();
+    }
+    /** Восстанавливает {@linkplain Calc#result} из файла {@linkplain Calc#FNAME}
+     * @throws Exception
+     */
+    public void restore() throws Exception {
+        ObjectInputStream is = new ObjectInputStream(new FileInputStream(FNAME));
+        result = (Item)is.readObject();
+        is.close();
+    }
 
 }
